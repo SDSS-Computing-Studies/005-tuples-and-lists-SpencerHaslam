c1f21183d41ebda3acede8d7a59ee0ce78c8ad0f
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-06 12:41:59 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 #!python3
 
 """
-Create a variable that contains an empy list.
+Create a variable that contains an empty list.
 Ask a user to enter 5 words.  Add the words into the list.
 Print the list
 inputs:
@@ -23,3 +23,9 @@ Enter a word: virus
 
 ['apple', 'worm', 'dollar', 'shingle', 'virus']
 """
+word1 = str(input("Enter a word"))
+word2 = str(input("Enter a word"))
+word3 = str(input("Enter a word"))
+word4 = str(input("Enter a word"))
+word5 = str(input("Enter a word"))
+print("['" + word1 + "', " + "'" + word2 + "', " + "'" + word3 + "', " + "'" + word4 + "', " + "'" + word5 + "']")
