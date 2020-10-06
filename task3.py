57333c837bd390a871c0934ada40e11f944a2941
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-06 13:36:56 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,6 +14,5 @@ The smallest number is 3 and the largest number is 9
 """
 
 myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
-minval = str(min(myList))
-maxval = str(max(myList))
-print("* " + minval + " .* " + maxval)
+myList.sort()
+print(myList)
